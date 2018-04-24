5419e06d59ee39a55966d29595e600849185cc8b
James <615283@users.noreply.github.com>
2018-04-24 09:01:42 +0100

    DictionaryUpdate timer

--- src/main/java/com/georlegacy/general/renamefilter/RenameFilter.java
+++ src/main/java/com/georlegacy/general/renamefilter/RenameFilter.java
@@ -2,12 +2,14 @@ package com.georlegacy.general.renamefilter;
 
 import com.georlegacy.general.renamefilter.commands.RFCommand;
 import com.georlegacy.general.renamefilter.events.RenameListener;
+import com.georlegacy.general.renamefilter.tasks.DictionaryUpdate;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.kohsuke.github.GitHub;
 
 import java.io.IOException;
+import java.util.Timer;
 
 public class RenameFilter extends JavaPlugin {
     private ConfigHandler configHandler = new ConfigHandler(this);
@@ -25,12 +27,18 @@ public class RenameFilter extends JavaPlugin {
         }
     }
 
+    private void updateDictionary() {
+        Timer t = new Timer();
+        t.scheduleAtFixedRate(new DictionaryUpdate(this), 0l, 1000*60*60*24);
+    }
+
     @Override
     public void onEnable() {
         PluginManager pm = Bukkit.getServer().getPluginManager();
         pm.registerEvents(new RenameListener(this), this);
         this.getCommand("rf").setExecutor(new RFCommand(this));
         getConfigHandler().load();
+        updateDictionary();
         this.loadGithub();
     }
 
